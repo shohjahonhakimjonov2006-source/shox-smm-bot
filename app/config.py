9d6d31113ace26aead98b3082799eb51505fc889
shohjahonhakimjonov2006-source <shohjahonhakimjonov2006@gmail.com>
2026-03-19 19:00:18 +0500

    Update config.py

--- app/config.py
+++ app/config.py
@@ -1,7 +1,10 @@
 import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 API_KEY = os.getenv("API_KEY")
 API_URL = os.getenv("API_URL")
 MONGO_URL = os.getenv("MONGO_URL")
-ADMIN_ID = int(os.getenv("ADMIN_ID"))
+ADMIN_ID = int(os.getenv("ADMIN_ID", 0))
