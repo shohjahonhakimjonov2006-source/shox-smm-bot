2ac93542d8125f1cea04f283c973746adcbcc893
shohjahonhakimjonov2006-source <shohjahonhakimjonov2006@gmail.com>
2026-03-19 19:00:41 +0500

    Update api.py

--- app/api.py
+++ app/api.py
@@ -3,22 +3,22 @@ from .config import API_URL, API_KEY
 
 async def create_order(service_id, link, qty):
     async with aiohttp.ClientSession() as session:
-        data = {
+        params = {
             "key": API_KEY,
             "action": "add",
             "service": service_id,
             "link": link,
             "quantity": qty
         }
-        async with session.post(API_URL, data=data) as r:
+        async with session.post(API_URL, params=params) as r:
             return await r.json()
 
 async def order_status(order_id):
     async with aiohttp.ClientSession() as session:
-        data = {
+        params = {
             "key": API_KEY,
             "action": "status",
             "order": order_id
         }
-        async with session.post(API_URL, data=data) as r:
+        async with session.post(API_URL, params=params) as r:
             return await r.json()
