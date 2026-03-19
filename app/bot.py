caf6e2b17d0656babb4a40d80909599c85305b65
shohjahonhakimjonov2006-source <shohjahonhakimjonov2006@gmail.com>
2026-03-19 19:01:14 +0500

    Update bot.py

--- app/bot.py
+++ app/bot.py
@@ -1,15 +1,24 @@
 import asyncio
+import logging
 from aiogram import Bot, Dispatcher
 from .config import BOT_TOKEN
 from .handlers import register
 
-bot = Bot(BOT_TOKEN)
+# Loglarni ko'rish uchun (xatolarni terminalda chiqaradi)
+logging.basicConfig(level=logging.INFO)
+
+bot = Bot(token=BOT_TOKEN)
 dp = Dispatcher()
 
+# Handlerlarni ro'yxatga olish
 register(dp, bot)
 
 async def main():
+    print("Bot ishga tushdi...")
     await dp.start_polling(bot)
 
 if __name__ == "__main__":
-    asyncio.run(main())
+    try:
+        asyncio.run(main())
+    except (KeyboardInterrupt, SystemExit):
+        print("Bot to'xtatildi")
