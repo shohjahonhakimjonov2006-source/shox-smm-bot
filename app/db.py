3c2af2ffa91baa3961ad87ecff73b1d78279adda
shohjahonhakimjonov2006-source <shohjahonhakimjonov2006@gmail.com>
2026-03-21 12:45:47 +0500

    Delete app directory

--- app/db.py
+++
@@ -1,9 +0,0 @@
-from motor.motor_asyncio import AsyncIOMotorClient
-from .config import MONGO_URL
-
-client = AsyncIOMocli = AsyncIOMotorClient(MONGO_URL)
-db = client["smm_ultra"]
-
-users = db["users"]
-orders = db["orders"]
-services = db["services"]
--- app/states.py
+++
@@ -1,9 +0,0 @@
-from aiogram.fsm.state import State, StatesGroup
-
-class OrderState(StatesGroup):
-    link = State()
-    qty = State()
-
-class PayState(StatesGroup):
-    amount = State()
-    receipt = State()
